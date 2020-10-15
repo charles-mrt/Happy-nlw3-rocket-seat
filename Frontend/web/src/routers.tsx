169e257e0864bb8d269ce09b23c66b7da0fcaf42
Charles <60299718+charles-mrt@users.noreply.github.com>
2020-10-15 17:12:01 -0300

    atualização dos arquivos

--- Frontend/web/src/routers.tsx
+++ Frontend/web/src/routers.tsx
@@ -4,13 +4,20 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
 
+import CreateOrphanage from './pages/CreateOrphanage';
+import Orphanage from './pages/Orphanage';
+
+
 function Routes() {
     return(
         <BrowserRouter>
-            <switch>
+            <Switch>
                 <Route path="/" exact component={Landing}/>
                 <Route path="/app" component={OrphanagesMap}/>
-            </switch>
+                
+                <Route path="/orphanages/create" component={CreateOrphanage}/>
+                <Route path="/orphanages/:id" component={Orphanage}/>
+            </Switch>
         </BrowserRouter>
     );
 }
